c749de759728e8351076bca2bfcaef26262b883f
Ratn Govindam <rg.ratn@gmail.com>
2023-11-12 22:36:51 +0530

    first commit wiht simple feature of text to speech wiht main.py file

---
+++ main.py
@@ -0,0 +1,16 @@
+#   Project Speaking Robo 
+import os
+
+if __name__ == '__main__':
+    print("\n\t\tWelcome to RoboSpeaker 1.1\t\t")
+    print("\t\t\t:- Created by RG-7")
+    print()
+    while True:
+        x = input("Enter what do you want me to speak : ")
+        if x.lower() == 'q':
+            os.system(f'PowerShell -Command "Add-Type –AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak(\'Thank You for using. See You soon!\');"')
+            break
+
+        command = f'PowerShell -Command "Add-Type –AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak(\'{x}\');"'
+        os.system(command)
+print()
